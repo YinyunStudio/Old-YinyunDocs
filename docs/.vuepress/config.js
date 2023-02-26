12c8ba2189ee59369c71effae8bdbfc80af5da59
XiaohCeale <3050155211@qq.com>
2023-02-26 19:28:36 +0800

    modified:   docs/.vuepress/config.js 	new file:   docs/.vuepress/styles/palette.styl 	modified:   package-lock.json 	modified:   package.json

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,17 +1,27 @@
 module.exports = {
     title: '阴云文档',
     description: '阴云项目的介绍与帮助文档',
+
+
     head: [
         ['link', {rel: 'icon', href: '/logo.png'}],
         ['meta', {name: 'keywords', content: '阴云,文档,帮助,说明,教程,介绍,阴云皮肤,渐蓝方块,vuepress'}],
         ['meta', {name: 'author', content: '阴云工作室,YinyunStudio'}],
     ],
+
+    
+    plugins: {
+        '@vuepress/medium-zoom': true
+    },
+
+    
     themeConfig: {
         nav: [
             { text: '阴云皮肤', link: '/yinyunskin/' },
             { text: '渐蓝方块', link: '/twobluecube/' },
             { text: '关于', link: '/about/' }
         ],
+
         sidebar: {
             '/yinyunskin/': [
               ['', '阴云皮肤']
@@ -25,7 +35,9 @@ module.exports = {
                 ['', '关于']
             ]
         },
-        displayAllHeaders: true, // 默认值：false
+
+        lastUpdated: '编辑于',
+        displayAllHeaders: true,
         smoothScroll: true
     }
 }
